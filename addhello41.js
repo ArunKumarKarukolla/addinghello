ce46155774b9661baef2fc1da9cce4d563b655dd
arun kumar <arunkumarpostbox2369@gmail.com>
2022-08-31 00:32:13 +0530

    first commit

---
+++ addhello41.js
@@ -0,0 +1,64 @@
+/*var itemList=document.querySelector('#items');
+//parent node
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundcolor='#f4f4f4';
+//console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+
+//childnode
+console.log(itemList.childNodes);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='#f4f4f4';
+
+//first child
+console,log(itemList.firstChild);
+
+//firstelementchild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent='Hello';
+
+//last child
+console,log(itemList.lastChild);
+//last Element child
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent='Hello';
+
+//next sibling
+console.log(itemList.nextSibling);
+
+//next Element child
+console.log(itemList.nextElementSibling);
+
+//previous siblimg
+console.log(itemList.previoustSibling);
+
+//previous element slibing
+console.log(itemList.nextElementSibling);
+
+
+//create element
+var newDiv=document.querySelector('div');
+newDiv.className='hello';
+//add id
+//newDiv.id='hello2';
+//add attribute
+//newDiv.setAttribute('title','hello');
+
+//create text node*/
+
+var newDiveText=document.createTextNode('HELLO WORLD');
+//ADD text to div
+
+newDive.appendChild(newDiveText);
+console.log(newDiv)
+var container=document.querySelector('header.container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+
+var itemList=document.getElementById('items');
+parentNode.innethtml='<li>item 1</li><li>item 2</li><li>item 3</li><li>item 4</li>'
+parentNode.innethtml='<li>hello wolrd</li>' +parentNode.innethtml
+console.log(itemList);
